646ca443b3d7dda53adc7fc3e13ea885e4a15f7a
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-04-22 20:14:50 +0900

    added file

---
+++ second/main.py
@@ -0,0 +1,21 @@
+# -* encoding: utf-8 *-
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import training, datasets, iterators, optimizers
+import numpy as np
+
+batch_size = 10
+
+class MNIST_Conv_MN(chainer.Chain):
+
+    def __init__(self):
+        super(MNIST_Conv_MN, self).__init__()
+
+
+def main():
+    print("main")
+
+
+if __name__ == '__main__':
+    main()
