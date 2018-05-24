c1e49a56e4ebe9755e20b8cacfa4f3a3ae8559db
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-05-25 00:38:34 +0900

    pipelineの設定開始

--- chapter4/gather_images/gather_images/pipelines.py
+++ chapter4/gather_images/gather_images/pipelines.py
@@ -5,7 +5,10 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import scrapy
+from scrapy.pipelines.images import ImagesPipeline
 
-class GatherImagesPipeline(object):
+# そのままだとSHA-1でハッシュ化したファイル名にするのでカスタマイズする
+class GatherImagesPipeline(ImagesPipeline):
     def process_item(self, item, spider):
         return item
