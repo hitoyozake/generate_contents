47c67a262a42927f0dd8b74701f06f82157b37ac
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-05-24 11:10:22 +0900

    日本語がだめっぽい

--- chapter4/gather_images/gather_images/spiders/googleimg.py
+++ chapter4/gather_images/gather_images/spiders/googleimg.py
@@ -1,7 +1,8 @@
 # -* encoding: utf-8 *-
 
 import scrapy
-
+import json
+import os
 
 class GoogleImageSpider(scrapy.Spider):
 
@@ -9,17 +10,33 @@ class GoogleImageSpider(scrapy.Spider):
 
     allowed_domains = ["google.com"]
 
+    start_urls = []
 
-    start_urls = ['https://www.google.com/search?q=%E3%83%90%E3%83%89%E3%82%AC%E3%83%BC%E3%83%AB+%E5%B7%A8%E4%B9%B3&biw=1739&bih=1179&gbv=1&tbm=isch&ei=WjAEW7PjGsXM0gSe9awg&start=1&sa=N',
-                  'https://www.google.com/search?q=%E3%83%90%E3%83%89%E3%82%AC%E3%83%BC%E3%83%AB+%E5%B7%A8%E4%B9%B3&biw=1739&bih=1179&gbv=1&tbm=isch&ei=WjAEW7PjGsXM0gSe9awg&start=20&sa=N',
-                  'https://www.google.com/search?q=%E3%83%90%E3%83%89%E3%82%AC%E3%83%BC%E3%83%AB+%E5%B7%A8%E4%B9%B3&biw=1739&bih=1179&gbv=1&tbm=isch&ei=WjAEW7PjGsXM0gSe9awg&start=40&sa=N',
-                  'https://www.google.com/search?q=%E3%83%90%E3%83%89%E3%82%AC%E3%83%BC%E3%83%AB+%E5%B7%A8%E4%B9%B3&biw=1739&bih=1179&gbv=1&tbm=isch&ei=WjAEW7PjGsXM0gSe9awg&start=60&sa=N'
-
-                  ]
+    def generate(self, keyword):
+        ar = []
+        for i in range(0, 8):
+            s = "https://www.google.com/search?q={0}&" \
+                "biw=1739&bih=1179&gbv=1&tbm=isch&ei=WjAEW7PjGsXM0gSe9awg&start={1}&sa=N".format(keyword, str(int(i*20)))
+            ar.append(s)
+        return ar
 
     def __init__(self):
         print('initialize')
         # もしもURLなどを初期化したいのならここで行う
+        root = os.path.dirname(os.path.abspath(__file__)) + "/"
+
+        with open(root+"start_urls.json") as f:
+            jsdata = json.loads(f.read(), 'shift_jis')
+
+            for i in jsdata:
+                if 'start_urls' in i:
+                    for url in i['start_urls']:
+                        GoogleImageSpider.start_urls.append(url)
+                if 'keywords' in i:
+                    for keyword in i['keyword']:
+                        ar = self.generate(keyword)
+                        for url in ar:
+                            GoogleImageSpider.start_urls.append(url)
 
     def parse(self, response):
         for url in response.xpath("//img/@src").extract():
