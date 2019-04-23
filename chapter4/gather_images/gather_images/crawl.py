cdcf347c4553237afcae09e67c2ef2e3a7132bb3
hitoyozake <mikoto_1122@yahoo.co.jp>
2019-04-23 23:28:46 +0900

    resolved

--- chapter4/gather_images/gather_images/crawl.py
+++ chapter4/gather_images/gather_images/crawl.py
@@ -11,5 +11,5 @@ jsonfiles = [ jsons for jsons in files if re.search(".*\.json$", jsons)]
 import subprocess
 
 for json in jsonfiles:
-    subprocess.call(["scrapy", "runspider", "./spiders/googleimg.py", "-a", "jsonfile="+json])
+    subprocess.call(["scrapy", "runspider", "./spiders/googleimg.py", "-a", "json_name="+json])
 
